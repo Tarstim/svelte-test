886846be014423a7ef72ba678ea3d2436233e050
Timofy <tarstim24@email.com>
2023-05-29 15:43:36 +0300

    ghgegr;

---
+++ src/routes/project/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+ <title>Портфолио</title>
+</svelte:head>
+
+<div class='container'>
+ <h1>Мои проекты</h1>
+ <div class='apps'>
+  {#each routes as page}
+   <div class='app'>
+    <h2>{page.title}</h2>
+    <p>{page.body}</p>
+    <p class='readmore'>
+     <a class='link' href={`/project/apps/${page.id}`}>
+      Читать далее
+     </a>
+    </p>
+   </div>
+  {/each}
+ </div>
+</div>
