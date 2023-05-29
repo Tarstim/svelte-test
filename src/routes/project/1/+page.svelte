9953eafaf65501bf3c4b185ced05670d045fe60a
Timofy <tarstim24@email.com>
2023-05-29 16:01:50 +0300

    thj

---
+++ src/routes/project/1/+page.svelte
@@ -0,0 +1,40 @@
+<div class='container'>
+    <a href='/project'>Домой</a>
+    <slot/>
+  </div>
+  
+  <style>
+    .container {
+      margin: 50px auto;
+      max-width: 800px;
+      padding: 0 20px;
+    }
+  </style>
+  
+  <script>
+</script>
+<main>
+    <form on:submit|preventDefault={add}>
+        <input bind:value={newItem} placeholder="Enter to-do" />
+        <button class="add-todo" on:click={add}><span>+</span></button>
+      </form>
+      
+</main>
+<style>
+    let newItem = '';
+let todoList = [];
+function add() {
+  if (newItem !== '') {
+    todoList = [
+      ...todoList,
+      {
+        задача: newItem,
+        завершено: false,
+      },
+    ];
+    newItem = '';
+  }
+}
+
+
+</style>
